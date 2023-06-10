11efc41ff5801c6dc3d110ab333b6a06d166a70f
SlyTy7 <tyler.west.dev@gmail.com>
2023-06-10 11:27:50 -0700

    add gitignore

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const stripe = require('stripe')('sk_test_51MN6VBJrPPXiJnm6eeTaKxhgBfMObAkSIBkV1qsIIuYndUrF3ZBeFAQn5NUGTi2en1Koc4DnmFPU3lTMZtx1fldU002tzdg8wb')
+const stripe = require('stripe')(process.env.STRIPE_PUBLISHABLE_KEY)
 
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req,res) => {
     res.send('Hello World!')
-})
-
-app.get('/checkout', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        success_url: 'https://example.com/success',
-        line_items: [
-            {price: 'price_1NHVNYJrPPXiJnm6KSYXdQoW', quantity: 2},
-        ],
-        mode: 'payment',
-    })
-
-    res.json({
-        id: session.id,
-        url: session.url
-    })
-
 })
 
 app.post('/checkout', async (req, res) => {
