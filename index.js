a784ddc1a55b7b667c922f770aa5352bc40a3ce2
SlyTy7 <tyler.west.dev@gmail.com>
2023-06-10 09:31:55 -0700

    add test req

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ app.get('/', (req,res) => {
 })
 
 app.get('/checkout', (req, res) => {
-    res.send('Checkout Links will be generated here.....')
+    const stripe = require('stripe')('sk_test_51MN6VBJrPPXiJnm6eeTaKxhgBfMObAkSIBkV1qsIIuYndUrF3ZBeFAQn5NUGTi2en1Koc4DnmFPU3lTMZtx1fldU002tzdg8wb');
+
+    const session = stripe.checkout.sessions.create({
+      success_url: 'https://example.com/success',
+      line_items: [
+        {price: 'price_H5ggYwtDq4fbrJ', quantity: 2},
+      ],
+      mode: 'payment',
+    });
+
+    res.send(session)
 })
 
 app.listen(process.env.PORT || 3000);
