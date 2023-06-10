28e25c40558df3446907ec3b75f858ba7182ae3b
SlyTy7 <tyler.west.dev@gmail.com>
2023-06-10 09:40:19 -0700

    move function

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+const stripe = require('stripe')('sk_test_51MN6VBJrPPXiJnm6eeTaKxhgBfMObAkSIBkV1qsIIuYndUrF3ZBeFAQn5NUGTi2en1Koc4DnmFPU3lTMZtx1fldU002tzdg8wb');
+const session = await stripe.checkout.sessions.create({
+    success_url: 'https://example.com/success',
+    line_items: [
+        {price: 'price_H5ggYwtDq4fbrJ', quantity: 2},
+    ],
+    mode: 'payment',
+})
 
 app.get('/', (req,res) => {
     res.send('Hello World!')
 })
 
 app.get('/checkout', (req, res) => {
-    const stripe = require('stripe')('sk_test_51MN6VBJrPPXiJnm6eeTaKxhgBfMObAkSIBkV1qsIIuYndUrF3ZBeFAQn5NUGTi2en1Koc4DnmFPU3lTMZtx1fldU002tzdg8wb');
-
-    const session = stripe.checkout.sessions.create({
-      success_url: 'https://example.com/success',
-      line_items: [
-        {price: 'price_H5ggYwtDq4fbrJ', quantity: 2},
-      ],
-      mode: 'payment',
-    });
-
+    res.write(session)
     res.send(session)
 })
 
